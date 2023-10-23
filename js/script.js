01b9fc363dec11ee75ffa1697c5a08bc72573985
teoma145 <matteodark145@yahoo.it>
2023-10-23 15:59:16 +0200

    array-object-team

---
+++ js/script.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const Team = [
+    {
+        name: 'Wayne Barnette',
+        role : 'Founder and Ceo',
+        picture:'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        name: 'Angela Caroll',
+        role : 'Chief Editor',
+        picture:'angela-carroll-chief-editor.jpg'
+    },
+    {
+        name: 'Walter Gordon',
+        role : 'Office Manager',
+        picture:'walter-gordon-office-manager.jpg'
+    },
+    {
+        name: 'Angela Lopez',
+        role : 'Social Media Manager',
+        picture:'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        name: 'Scott Estrada',
+        role : 'Developer',
+        picture:'scott-estrada-developer.jpg'
+    },
+    {
+        name: 'Barbara Ramos',
+        role : 'Graphic Designer',
+        picture:'barbara-ramos-designer.jpg'
+    },
+
+];
+
